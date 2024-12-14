692718b45a719307554c5b3f2fc354c439101642
Udemy - KhoaPham <freedom3000@gmail.com>
2024-12-14 18:43:36 +0700

    feat: 01-object

---
+++ tests/lesson-3/01-object.js
@@ -0,0 +1,54 @@
+// 1
+// let car = { make: "Toyota", modal: "Corolla", year: 2021 };
+// console.log(car);
+
+//2
+// let person = {
+//     name: "A",
+//     address: {
+//         street: "CN11",
+//         city: "HCM",
+//         country: "VN"
+//     }
+// };
+// console.log(person.address["street"]);
+
+//3
+// let student = {
+//     name: "B",
+//     grades: {
+//         math: 10,
+//         english: 8
+//     }
+// };
+// console.log(student["grades"]["math"]);
+
+//4
+
+//5
+// let setting = {
+//     volume: 10,
+//     brightness: 50
+// };
+// setting.volume = 15;
+// console.log(setting.volume);
+
+//6
+// let bike = {};
+// bike.color = "red";
+// console.log(bike);
+
+//7
+// let employee = {
+//     name: "C",
+//     age: 29
+// };
+// delete employee.age;
+// console.log(employee);
+
+//8
+const school = {
+    classA: ['A', 'A1', 'A2'],
+    classB: ['B', 'B2', 'B3'],
+}
+console.log(school);
